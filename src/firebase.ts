20a068abcd31f0785b96caa9f458c440b606933c
Isaac Mtsiriza <230100836@luanar.ac.mw>
2026-07-12 12:57:09 +0200

    Fix Firebase config for YAVA

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,13 +1,22 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { initializeFirestore } from 'firebase/firestore';
-import config from '../firebase-applet-config.json';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { initializeFirestore } from "firebase/firestore";
+import config from "../firebase-applet-config.json";
 
-// Initialize Firebase App
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const app = initializeApp(config);
+const analytics = getAnalytics(app);
 
 // Initialize Firebase Auth
 export const auth = getAuth(app);
 
-// Initialize Firestore with custom databaseId if specified, else use '(default)'
-export const db = initializeFirestore(app, {}, config.firestoreDatabaseId || '(default)');
+// Initialize Firestore
+export const db = initializeFirestore(app, {}, "(default)");
+
+export { app, analytics };
